7a5e49c99c8d736bea06b2721c5b8c4635d52015
Owner <@femidavid10>
2024-01-29 03:49:36 +0100

    initial commit

---
+++ android/app/src/main/kotlin/femidavid/com/i_am_poor/MainActivity.kt
@@ -0,0 +1,6 @@
+package femidavid.com.i_am_poor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('I Am Poor'),
+          backgroundColor: Colors.grey[900],
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/icons8-coal-96.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
